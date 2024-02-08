22387d1d53bfe0d0ea98622699fa93c571f57c94
Thalita <thalita_dos-santos@estudante.sesisenai.org.br>
2024-02-08 11:06:50 -0300

    adicionado a classe e os estilos

---
+++ voto_class.js
@@ -0,0 +1,8 @@
+class Eleitor{
+    constructor(nome, idade, titulo){
+        this.nome = nome
+        this.idade = idade
+        this.titulo = titulo
+         
+    }
+}
--- voto_main.js
+++ voto_main.js
@@ -6,5 +6,12 @@ verificar.addEventListener('click',()=>{
     let idade = Number(document.getElementById('idade').value)
     let titulo = Number(document.getElementById('titulo').value)
 
-    console.log(nome,idade,titulo)
+    console.log("dados do formulário: ",nome,idade,titulo)
+
+    const cidadao = new Eleitor()
+    cidadao.nome = nome 
+    cidadao.idade = idade
+    cidadao.titulo = titulo
+
+    console.log("Instância do objeto:",cidadao)
 })
